b3abc676750a6bcd8dbdab7d6341478854eb2734
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-12-04 10:35:46 +0100

    Some bug fixes. Thanks @senthil10 for your help

--- roles/nas/files/archive.sh
+++ roles/nas/files/archive.sh
@@ -3,10 +3,24 @@
 d=`date`
 year=${d: -2}
 
-if ls -d ${year}* &> /dev/null
+# Look for uncompressed runs
+if ls -d ${year}*/ &> /dev/null
 then
-    for run in `ls -d ${year}*`
+    for run in `ls -d ${year}*/`
     do
         pm archive swestore --package-run --pbzip2 --check-finished --send-to-swestore --swestore-path a2010002 --flowcell $run --clean-swestore --force --clean-from-staging &
     done
+else
+    echo "No runs found to tarball :-)"
+fi
+
+# Look for already compressed runs
+if ls ${year}*bz2 &> /dev/null
+then
+   for run in `ls ${year}*bz2`
+    do
+        pm archive swestore --send-to-swestore --swestore-path a2010002 --tarball $run --clean-swestore --clean-from-staging &
+    done
+else
+    echo "No tarballed runs found :-)"
 fi
